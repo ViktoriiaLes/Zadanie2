f0003efd47837b57d76e86a193ef48b4cd16a1a7
ViktoriiaLes <55627011+ViktoriiaLes@users.noreply.github.com>
2019-10-01 08:55:50 +0200

    Update Drink.java
    Add  ingredients

--- Drink/src/Drink.java
+++ Drink/src/Drink.java
@@ -2,10 +2,9 @@ public class Drink {
     String name;
     double price;
     boolean isAlcohol;
-
-    Ingredient ingredient1 = new Ingredient();
-    Ingredient ingredient2 = new Ingredient();
-    Ingredient ingredient3 = new Ingredient();
+    Ingredient ingredient1;
+    Ingredient ingredient2; 
+    Ingredient ingredient3; 
 
     public double getTotalVolume() {
                return ingredient1.volume + ingredient2.volume + ingredient3.volume;
